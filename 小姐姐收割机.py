5c75edb9f605c3e9428766e2feb27a7ecb2c82bb
Gewöhnlicher Programmierer <86613239+zzk666666@users.noreply.github.com>
2022-01-23 17:36:42 +0800

    Create 小姐姐收割机.py

---
+++ 小姐姐收割机.py
@@ -0,0 +1,32 @@
+import requests
+from lxml import etree
+from urllib import request
+import time
+
+#1.目标  定义虎牙
+url='https://www.huya.com/g/2168'
+
+#2.请求  网络请求<Response [200]>
+src=requests.get(url)
+#编译
+html=etree.HTML(src.text)
+
+#3.数据  所需数据
+id_list=html.xpath('.//img[@class="pic"]')
+
+#4.下载  下载保存
+for data in id_list:
+    img=data.xpath('./@data-original')[0]
+    #print(img)
+    img=img.split('?')[0]
+    print(img)
+    name=data.xpath('./@alt')[0]
+    #print(name)
+
+    file = '虎牙美女文件夹/' + name +'.jpg'
+    request.urlretrieve(img,file)
+    requests.get(url)
+
+    print('<%s>下载完成!'% name)
+
+    time.sleep(1)
